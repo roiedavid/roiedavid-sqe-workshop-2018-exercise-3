4e88894c20010c3d1af31ba72be77467430e7d71
Roie David <roiedavid47@gmail.com>
2018-12-30 17:24:57 +0200

    first support of coloring path

--- src/js/app.js
+++ src/js/app.js
@@ -7,16 +7,17 @@ $(document).ready(function () {
         let codeToParse = $('#codePlaceholder').val(), argumentsToParse = $('#argumentsPlaceholder').val();
         let parsedCode = parseCode(codeToParse), parsedArgs = parseCode(argumentsToParse);
         $('#parsedCode').val(JSON.stringify(parsedCode, null, 2));
-        let nodesString = getFunctionNodesString(parsedCode);
+        let nodesString = getFunctionNodesString(parsedCode, parsedArgs);
         let edgesString = getFunctionEdgesString(parsedCode);
         console.log(nodesString + '\n' + edgesString);
         let graph = flowchart.parse(nodesString + '\n' + edgesString);
         clearGraph();
         graph.drawSVG('graph', {
             'flowstate': {
-                'connection': {'font-color': 'white'},
+                'inConnection': {'fill': 'Chartreuse', 'font-color': 'Chartreuse'},
+                'outConnection': {'font-color': 'white'},
                 'inPath': {'fill': 'Chartreuse', 'yes-text': 'T', 'no-text': 'F'},
-                'ifAndFalse': {'yes-text': 'T', 'no-text': 'F'}
+                'outPath': {'yes-text': 'T', 'no-text': 'F'}
             }
         });
     });
--- src/js/code-analyzer.js
+++ src/js/code-analyzer.js
@@ -1,5 +1,6 @@
 import * as esprima from 'esprima';
 import * as escodegen from 'escodegen';
+import * as estraverse from 'estraverse';
 
 let opIndex, condIndex, paraIndex, connIndex, newOP, lines;
 
@@ -13,16 +14,38 @@ const astToCode = (ast) => {
     return escodegen.generate(ast);
 };
 
+function getParams(parsedFunction) {
+    let params = [];
+    for (let i = 0 ; i < parsedFunction.params.length; i++)
+        params.push(astToCode(parsedFunction.params[i])); // params is array of the function's params names
+    return params;
+}
+
+function getArgsValues(parsedArgs) {
+    let expression = parsedArgs.body[0].expression;
+    if (expression.type === 'SequenceExpression') // case for more than one arguments
+        return expression.expressions.map(exp => eval(astToCode(exp)));
+    return [eval(astToCode(expression))]; // only one argument
+}
+
+function generateBindings(params, args) {
+    let bindings = {};
+    for (let i = 0 ; i < params.length; i++)
+        bindings[params[i]] = args[i];
+    return bindings;
+}
+
 // Nodes Building
-function getFunctionNodesString(parsedCode) {
+function getFunctionNodesString(parsedCode, parsedArgs) {
     initGlobalIndexes();
+    let bindings = generateBindings(getParams(parsedCode.body[0]), getArgsValues(parsedArgs));
     let body = parsedCode.body[0].body.body, nodesString = '', firstCond = true;
     for(let  i = 0; i < body.length; i++){
         if ((body[i].type === 'IfStatement' || body[i].type === 'WhileStatement') && firstCond){
-            nodesString = `op${opIndex++}=>operation: ${nodesString}`;
+            nodesString = `op${opIndex++}=>operation: ${nodesString}|inPath\n`;
             firstCond = false;
         }
-        nodesString += getNodeString(body[i]);
+        nodesString += getNodeString(body[i], bindings, true);
     }
     lines = nodesString.split('\n');
     return nodesString;
@@ -33,18 +56,55 @@ const getStringHandlersMap = {'IfStatement': getIfStatementNodeString, 'WhileSta
     'AssignmentExpression': getAssignmentExpressionNodeString, 'VariableDeclaration': getVariableDeclarationNodeString,
     'ReturnStatement': getReturnStatementNodeString};
 
-function getNodeString(node) {
+function getNodeString(node, bindings, inPath) {
     let func = getStringHandlersMap[node.type];
-    return func ? func(node) : '';
+    return func ? func(node, bindings, inPath) : '';
 }
 
-function getIfStatementNodeString(ifStatement) {
+function getIfStatementNodeString(ifStatement, bindings, inPath) {
     newOP = true;
     let nodesString = `cond${condIndex++}=>condition: ${astToCode(ifStatement.test)}\n`;
-    nodesString += `para${paraIndex++}=>parallel: ${getBlockContent(ifStatement.consequent)}`;
-    nodesString += `conn${connIndex++}=>start: i'm white|connection\n`;
-    nodesString += ifStatement.alternate ? getNodeString(ifStatement.alternate) : '';
+    if(inPath)
+        nodesString += '|inPath\n';
+    let testRes = evalTest(ifStatement.test, bindings);
+    nodesString += `para${paraIndex++}=>parallel: ${getBlockContent(ifStatement.consequent)}${testRes ? '|inPath\n' : ''}`;
+    nodesString += `conn${connIndex++}=>start: empty|${inPath ? 'in' : 'out'}Connection\n`;
+    nodesString += ifStatement.alternate ? getNodeString(ifStatement.alternate, deepCopy(bindings), !testRes) : '';
     return nodesString;
+}
+
+function evalTest(test, bindings) {
+    let newTest = deepCopy(test);
+    newTest = replaceTestVariablesByValues(newTest, bindings);
+    return eval(astToCode(newTest));
+}
+
+function replaceTestVariablesByValues(test, bindings){
+    return estraverse.replace(test, {
+        enter: function (node) {
+            if (node.type === 'Identifier'){
+                let value = bindings[astToCode(node)];
+                try {
+                    value = eval(value);
+                }
+                catch (error) {
+                    value = eval(astToCode(getValueAsLiterals(value, bindings)).replace(/;/g, ''));
+                }
+                return parseCode(value.toString()).body[0].expression;
+            }
+        }
+    });
+}
+
+function getValueAsLiterals(value, bindings) {
+    let ast = parseCode(value).body[0];
+    estraverse.replace(ast, {
+        enter: function (node) {
+            if (node.type === 'Identifier')
+                return parseCode(bindings[node.name].toString()).body[0];
+        }
+    });
+    return ast;
 }
 
 function getBlockContent(node) {
@@ -57,27 +117,29 @@ function getBlockContent(node) {
     return astToCode(node).replace(/;/g,'') + '\n';
 }
 
-function getWhileStatementNodeString(whileStatement) {
+function getWhileStatementNodeString(whileStatement, bindings, inPath) {
     newOP = true;
     let nodesString = `op${opIndex++}=>operation: NULL\n`;
     nodesString += `cond${condIndex++}=>condition: ${astToCode(whileStatement.test)}\n`;
-    nodesString += `para${paraIndex++}=>parallel: ${getBlockContent(whileStatement.body)}`;
+    nodesString += `para${paraIndex++}=>parallel: ${getBlockContent(whileStatement.body, bindings, inPath)}`;
     return nodesString;
 }
 
-function getBlockStatementNodeString(blockStatement) {
+function getBlockStatementNodeString(blockStatement, bindings, inPath) {
     let nodesString = '';
     for (let i = 0 ; i < blockStatement.body.length; i++)
-        nodesString += getNodeString(blockStatement.body[i]);
+        nodesString += getNodeString(blockStatement.body[i], bindings, inPath);
     newOP = true;
     return nodesString;
 }
 
-function getExpressionStatementNodeString(expressionStatement) {
-    return getNodeString(expressionStatement.expression);
+function getExpressionStatementNodeString(expressionStatement, bindings, inPath) {
+    return getNodeString(expressionStatement.expression, bindings, inPath);
 }
 
-function getAssignmentExpressionNodeString(assignmentExpression){
+function getAssignmentExpressionNodeString(assignmentExpression, bindings, inPath){
+    if(inPath)
+        bindings[assignmentExpression.left.name] = astToCode(assignmentExpression.right).replace(/;/g,'');
     let content = astToCode(assignmentExpression).replace(/;/g, '') + '\n';
     if(newOP)
         content = `op${opIndex++}=>operation: ` + content;
@@ -85,12 +147,14 @@ function getAssignmentExpressionNodeString(assignmentExpression){
     return content;
 }
 
-function getVariableDeclarationNodeString(variableDeclaration) {
+function getVariableDeclarationNodeString(variableDeclaration, bindings) {
+    for (let i = 0; i< variableDeclaration.declarations.length; i++)
+        bindings[variableDeclaration.declarations[i].id.name] = astToCode(variableDeclaration.declarations[i].init).replace(/;/g,'');
     return astToCode(variableDeclaration).replace(/let |;/g, '') + '\n';
 }
 
 function getReturnStatementNodeString(returnStatement){
-    return `op${opIndex++}=>operation: ${astToCode(returnStatement).replace(/;/g, '')}\n`;
+    return `op${opIndex++}=>operation: ${astToCode(returnStatement).replace(/;/g, '')}|inPath\n`;
 }
 
 // Edges Building
@@ -175,6 +239,10 @@ function getWhileStatementEdgeString() {
 function initGlobalIndexes() {
     opIndex = condIndex = paraIndex = connIndex = 1;
     newOP = true;
+}
+
+function deepCopy(element) {
+    return JSON.parse(JSON.stringify(element));
 }
 
 export {parseCode, getFunctionNodesString, getFunctionEdgesString};
